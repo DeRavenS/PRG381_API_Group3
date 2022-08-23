a7ebf5bd4ad47eb670547209898ddcfefef35d9e
GhostForceZA <gustavdp2001@gmail.com>
2022-08-23 17:54:25 +0200

    Added security
    Added the dependencies and security config

---
+++ src/main/java/com/springboot/App/DataAccessLayer/config/securityConfig.java
@@ -0,0 +1,63 @@
+package com.springboot.App.DataAccessLayer.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.springboot.App.DataAccessLayer.service.StudentsService;
+
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+	private StudentsService studentService;
+	
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService((UserDetailsService) studentService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers(
+            "/registration**",
+            "/js/**",
+	        "/css/**",
+	        "/img/**"
+        ).permitAll()
+        .anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout()
+		.invalidateHttpSession(true)
+		.clearAuthentication(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/login?logout")
+		.permitAll();
+    }
+}
