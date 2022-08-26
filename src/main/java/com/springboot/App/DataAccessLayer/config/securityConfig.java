de0993deceb23c03afa1c7a36ae3618f51d86026
GhostForceZA <gustavdp2001@gmail.com>
2022-08-26 14:04:24 +0200

    Added security back
    Added security config file back

--- src/main/java/com/springboot/App/DataAccessLayer/config/securityConfig.java
+++ src/main/java/com/springboot/App/DataAccessLayer/config/securityConfig.java
@@ -1,23 +1,26 @@
 package com.springboot.App.DataAccessLayer.config;
-/* package com.springboot.App.DataAccessLayer.config;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 import com.springboot.App.DataAccessLayer.service.StudentsService;
 
 
 @Configuration
 @EnableWebSecurity
-public class securityConfig extends WebSecurityConfigurerAdapter {
+public class securityConfig  {
 
     @Autowired
 	private StudentsService studentService;
@@ -35,31 +38,26 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
         return auth;
     }
 	
-	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    @Bean
+    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
+        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
+            EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
+        contextSourceFactoryBean.setPort(0);
+        return contextSourceFactoryBean;
     }
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers(
-            "/registration**",
-            "/js/**",
-	        "/css/**",
-	        "/img/**"
-        ).permitAll()
-        .anyRequest().authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/login")
-		.permitAll()
-		.and()
-		.logout()
-		.invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutSuccessUrl("/login?logout")
-		.permitAll();
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    private Customizer<HttpBasicConfigurer<HttpSecurity>> withDefaults() {
+        return null;
     }
 }
- */
